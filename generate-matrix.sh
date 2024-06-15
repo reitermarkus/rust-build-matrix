95dacd2632ae166e0c42bd565a16a9013ba99541
Markus Reiter <me@reitermark.us>
2024-06-15 16:35:04 +0200

    Debug CI.

--- generate-matrix.sh
+++ generate-matrix.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-env | sort
+set -x
 
 if ! targets="$(cargo +nightly -Z unstable-options config get build.target --format json 2>/dev/null | jq .build.target)"; then
   targets='["x86_64-unknown-linux-gnu", "x86_64-apple-darwin", "aarch64-apple-darwin"]'
